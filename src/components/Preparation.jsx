8438f31b896ca4787e438fb688ba288fbba0bed9
IstiakAR <istiakrhyme840@gmail.com>
2025-06-10 13:54:24 +0600

    added loading animation

--- src/components/Preparation.jsx
+++ src/components/Preparation.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState, useRef } from 'react';
 import '../styles/Preparation.css';
-import { shuffle } from 'lodash';
+import { set, shuffle } from 'lodash';
 import GameResult from './GameResult.jsx';
+import loadingIcon from '../assets/loading.svg';
 
 function Preparation({numberCards}) {
   const [images, setImages] = useState([]);
   const urlsRef = useRef([]);
   const numberOfCards = numberCards;
+  const [loading, setLoading] = useState(true);
 
   const [score, setScore] = useState(new Set());
   const [gameOver, setGameOver] = useState(false);
@@ -34,6 +36,7 @@ function Preparation({numberCards}) {
 
   useEffect(() => {
     const fetchImages = async () => {
+      setLoading(true);
       const imagePromises = [];
       for(let i=0; i<numberOfCards;i++){
         const char = String.fromCharCode('A'.charCodeAt(0) + i);
@@ -54,6 +57,7 @@ function Preparation({numberCards}) {
       const results = await Promise.all(imagePromises);
       const validImages = results.filter(img => img !== null);
       setImages(validImages);
+      setLoading(false);
     };
     
     fetchImages();
@@ -61,9 +65,10 @@ function Preparation({numberCards}) {
     return () => {
       urlsRef.current.forEach(url => URL.revokeObjectURL(url));
       urlsRef.current = [];
+      setImages([]);
     };
     
-  }, []);
+  }, [numberOfCards]);
 
   if (score.size == numberOfCards || gameOver) {
     return <GameResult target={numberOfCards} score={score.size} highscore={highScore} gameResult={gameOver}/>;
@@ -71,21 +76,30 @@ function Preparation({numberCards}) {
 
   return (
     <div>
-      <div className='scoreboard'>
-        <span className='score score-text'>Current Score: {score.size}</span>
-        <span className='score score-text'>Target: {numberOfCards}</span>
-        <span className='score score-text'>Highest Score: {highScore}</span>
-      </div>
-      <div className="card-container">
-        {shuffle(images).map(({ char, url }) => (
-          <div key={char} className="robot-card" onClick={() => handleClick(char)}>
-            <img key={char} src={url} alt={`Robot ${char}`} />
-            <span className='card-text'>{char}</span>
+      {loading ? (
+        <div className='loading'>
+          <img src={loadingIcon} alt="Loading..." />
+        </div>
+      ) : (
+        <>
+          <div className='scoreboard'>
+            <span className='score score-text'>Current Score: {score.size}</span>
+            <span className='score score-text'>Target: {numberOfCards}</span>
+            <span className='score score-text'>Highest Score: {highScore}</span>
           </div>
-        ))}
-      </div>
+          <div className="card-container">
+            {shuffle(images).map(({ char, url }) => (
+              <div key={char} className="robot-card" onClick={() => handleClick(char)}>
+                <img key={char} src={url} alt={`Robot ${char}`} />
+                <span className='card-text'>{char}</span>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
+
 }
 
 export default Preparation;
